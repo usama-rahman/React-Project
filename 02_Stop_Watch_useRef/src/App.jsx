9a8a20569c9621f927ce2646dd82d63533a07da8
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-28 22:11:01 +0600

    Seach feature

--- 02_Stop_Watch_useRef/src/App.jsx
+++ 02_Stop_Watch_useRef/src/App.jsx
@@ -1,5 +1,34 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { useState, useRef } from "react";
 
-export default App;
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  );
+}
