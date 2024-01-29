a519bc4f6a48ac1302e3314ca034d301f2621fad
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-29 21:25:27 +0600

    Projects

--- 04-navbarReact/src/Navbar.jsx
+++ 04-navbarReact/src/Navbar.jsx
@@ -1,8 +1,3 @@
-// import React, { useState, useRef, useEffect } from "react";
-// import { FaBars, FaTwitter } from "react-icons/fa";
-// import { links, social } from "./data";
-// import logo from "./logo.svg";
-
 const Navbar = () => {
   return <h4>navbar</h4>;
 };
