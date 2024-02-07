32870b2eebc5ccdc70b5f77d51123b3316078e4e
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-08 04:54:23 +0600

    john-smilga

--- 04-navbarReact/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
-  );
-}
-
-export default App;
--- 04-navbarReact/src/Navbar.jsx
+++
@@ -1,5 +0,0 @@
-const Navbar = () => {
-  return <h4>navbar</h4>;
-};
-
-export default Navbar;
